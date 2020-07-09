64a4720ece03dcbb73cb1d5d9dae25ac51bb47e8
Utkarsh Gupta <utkarsh@debian.org>
2020-07-09 15:05:18 +0530

    Refactor #path method
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- lib/get_root.rb
+++ lib/get_root.rb
@@ -4,21 +4,21 @@ require "get_root/version"
 
 module GetRoot
   class << self
-    $fallback_root = Dir.pwd
-    def path(count = 0, relative_path = ".", root = nil)
-      if count > 10
-        root = $fallback_root
-        puts "Assuming root directory to be #{root}. " \
-             "Please run the same command from the root directory of the project."
-        return root
-      end
-      if Dir.entries(relative_path).include?(".git")
+    def path(count = 0, relative_path = +".", root = nil)
+      if count > 15
         root = File.expand_path(relative_path)
+        # puts "Assuming root directory to be #{root}. " \
+        #      "Please run the same command from the root directory of the project."
         return root
       else
-        relative_path.prepend("../")
-        count += 1
-        path(count, relative_path)
+        if Dir.entries(relative_path).include?(".git")
+          root = File.expand_path(relative_path)
+          return root
+        else
+          relative_path.prepend("../")
+          count += 1
+          path(count, relative_path)
+        end
       end
     end
   end
