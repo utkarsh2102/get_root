74d36a36f8a3628b0c36cfd6ba96678bca1df30e
Utkarsh Gupta <utkarsh@debian.org>
2020-07-09 17:27:07 +0530

    Write a test
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- spec/get_root_spec.rb
+++ spec/get_root_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require "get_root"
+
 RSpec.describe GetRoot do
   it "has a version number" do
     expect(GetRoot::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "shows the root directory correctly" do
+    root_path = GetRoot.path
+    relative_path = File.expand_path(".")
+    expect(root_path).to eq(relative_path)
   end
 end
