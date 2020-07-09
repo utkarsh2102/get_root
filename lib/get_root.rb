1c4ba39d80d96a3fc6bc6c5da3f92a0012849e4a
Utkarsh Gupta <utkarsh@debian.org>
2020-07-09 11:55:08 +0530

    Implement GetRoot module
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- lib/get_root.rb
+++ lib/get_root.rb
@@ -3,6 +3,23 @@
 require "get_root/version"
 
 module GetRoot
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    $fallback_root = Dir.pwd
+    def path(count = 0, relative_path = ".", root = nil)
+      if count > 10
+        root = $fallback_root
+        puts "Assuming root directory to be #{root}. " \
+             "Please run the same command from the root directory of the project."
+        return root
+      end
+      if Dir.entries(relative_path).include?(".git")
+        root = File.expand_path(relative_path)
+        return root
+      else
+        relative_path.prepend("../")
+        count += 1
+        path(count, relative_path)
+      end
+    end
+  end
 end
